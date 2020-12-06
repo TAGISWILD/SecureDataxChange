8bbb1fa84049b9a9d301d2375df4c6a6f2ebd551
Atharva Chauhan <46600294+TAGISWILD@users.noreply.github.com>
2020-12-06 14:30:53 +0530

    Added Main Files

---
+++ AboutUs.bat
@@ -0,0 +1,3 @@
+start http://aysoat.unaux.com/?i=2
+start http://www.aysoatstore.blogspot.com
+start https://aysoat.business.site
---
+++ Res.bat
@@ -0,0 +1,3 @@
+attrib -h -s ftp.aysoatdat
+attrib -h -s name.aysoatdat
+attrib -h -s Set_me_up_first.exe
